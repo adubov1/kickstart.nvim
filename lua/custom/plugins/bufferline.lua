d55dbc1a16b9f9168c14c9738ae4fdb9df6bc0d8
Alex Dubov <dubov@wisc.edu>
2026-01-22 14:56:05 -0600

    add more bufferline mappings, add nvim-colorizer, move ruby-lsp to vim.lsp

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -10,15 +10,13 @@ return {
     config = function(_, opts)
       require('bufferline').setup(opts)
       local map = vim.keymap.set
-
       map('n', '<leader>bH', '<Cmd>BufferLineCloseLeft<CR>', { desc = 'Delete buffers to the left' })
       map('n', '<leader>bL', '<Cmd>BufferLineCloseRight<CR>', { desc = 'Delete buffers to the right' })
-
+      map('n', '<leader>bO', '<Cmd>BufferLineCloseLeft<CR><Cmd>BufferLineCloseRight<CR>', { desc = 'Close all other buffers' })
       map('n', 'H', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Switch to the previous buffer' })
       map('n', 'L', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Switch to the next buffer' })
-
-      map('n', '<leader>b[', '<Cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer left' })
-      map('n', '<leader>b]', '<Cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer right' })
+      map('n', '<C-H>', '<Cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer left' })
+      map('n', '<C-L>', '<Cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer right' })
     end,
   },
 }
